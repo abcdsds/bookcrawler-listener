0f8212809b5cec6096ff7dd65255229398f07df9
abcdefg <choiyurim2@naver.com>
2020-07-03 23:01:15 +0900

    spring batch guide - 1,2

--- src/main/java/bookcrawlerconsumer/crawler/KafkaCrawlerListener.java
+++ src/main/java/bookcrawlerconsumer/crawler/KafkaCrawlerListener.java
@@ -2,26 +2,34 @@ package bookcrawlerconsumer.crawler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.util.SerializationUtils;
 
+import bookcrawlerconsumer.common.message.ScrapingMessage;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class KafkaCrawlerListener {
 
+
 	@KafkaListener(topics = "crawler-topic", containerFactory = "crawlerKafkaListenerContainerFactory")
 	public void listenMessage(List<byte[]> datas) {
+		
+		List<ScrapingMessage> messages = new ArrayList<>();
 		
 		log.info("null check : {}", datas.get(0) == null);
 		log.info("type check : {}", datas.get(0) instanceof byte[]);
 		
 		for (byte[] data : datas) {
-			System.out.println(data);
+			messages.add((ScrapingMessage)SerializationUtils.deserialize(data));
 		}
+		
+		
+
 		
 	}
 
